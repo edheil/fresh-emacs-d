52a5961ac8ddeb20e3fcacbd1a3a30844064c778
Ed Heil <eheil@fusionary.com>
2013-03-05 14:49:32 -0500

    packages

--- init.el
+++ init.el
@@ -2,3 +2,9 @@
 
 (when (display-graphic-p)
   (load-file "~/.emacs.d/windowed.el"))
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
